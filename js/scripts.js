e5cdbf51a95f3f28f72cab1b944d5021595b80f9
tiit-win <tiitkroon@gmail.com>
2020-06-12 16:31:56 +0300

    terms check

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ var input = document.querySelector(".phone");
 window.intlTelInput(input, {
   // any initialisation options go here
 });
+
+function checkSubmit() {
+  if(document.getElementById('terms').checked) {
+     return true; 
+  } else {
+    alert('Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy'); 
+    return false;
+  }
+}
